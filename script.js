06ad610de9ebfb916529a7622e618ce85fc0d7bc
Josh Rufin <joshrufin@gmail.com>
2017-04-17 13:48:24 -0400

    edited

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var temperature = 0;
 function convertTemperature(temperature, degree){
 if (degree === "C") {
-  temperature = (temperature*1.8)+32;
-  console.log (temperature + "F");
+    temperature = ((temperature-32)*1.8);
+  console.log (temperature + " F");
 }
 
 if (degree === "F"){
-  (temperature-32)*1.8;
-  console.log(temperature + "C");
+  temperature = ((temperature*1.8)+32);
+  console.log(temperature + " C");
 }
 }
 
-convertTemperature(18, "F");
+convertTemperature(212, "C");
+convertTemperature(32, "C");
+convertTemperature(65, "C");
+convertTemperature(0, "F");
