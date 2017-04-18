b1fddab05acd24633852528bc3d0b249567d9b2b
Josh Rufin <joshrufin@gmail.com>
2017-04-18 09:10:52 -0400

    fixed

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function convertTemperature(temperature, degree){
 if (degree === "C") {
     temperature = ((temperature-32)*1.8);
-  console.log (temperature + " F");
+  console.log (temperature + " C");
 }
 
 if (degree === "F"){
   temperature = ((temperature*1.8)+32);
-  console.log(temperature + " C");
+  console.log(temperature + " F");
 }
 }
 
@@ -14,3 +14,19 @@ convertTemperature(212, "C");
 convertTemperature(32, "C");
 convertTemperature(65, "C");
 convertTemperature(0, "F");
+
+function guessGame() {
+    var number = Math.random() * 10 | 1,
+        guess,
+        text = 'Guess a number:';
+    do {
+        guess = prompt(text);
+        if (number < guess) {
+            text = "You've guessed too high!";
+        } else if (number > guess) {
+            text = "You've guessed too low!";
+        }
+    } while (guess != number);
+    document.write("Good Work");
+}
+guessGame();
